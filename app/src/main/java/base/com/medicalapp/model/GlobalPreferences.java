d87c01cc3640c3382443a855344542df59ed7f1f
vn90094 <ramachandran.ravisankar@heb.com>
2017-01-17 16:24:05 -0600

    Order create method added
    Added method to create an order
    Place order button in cart

--- app/src/main/java/base/com/medicalapp/model/GlobalPreferences.java
+++ app/src/main/java/base/com/medicalapp/model/GlobalPreferences.java
@@ -1,6 +1,18 @@
 package base.com.medicalapp.model;
 
+import android.content.Context;
+import android.widget.Toast;
+
+import com.google.gson.Gson;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
+
+import base.com.medicalapp.manager.ApiResponseWrapper;
+import base.com.medicalapp.manager.NetworkManager;
+import cz.msebera.android.httpclient.entity.StringEntity;
 
 /**
  * Created by ctsuser1 on 1/16/17.
@@ -8,14 +20,18 @@ import java.util.List;
 
 public class GlobalPreferences {
     private static GlobalPreferences mInstance = null;
-
+    private static final String ORDERS_URL_BASE
+            = "Orders?api_key=keyOKgBm0Ho3UFLs6";
     private String retailerId;
     private List<OrderFields> cartProducts;
     private String orderId;
+    private static Context context;
+    private OrderItemRecords orderItemrecord;
 
     private GlobalPreferences(){
         retailerId = "0";
         orderId = "0";
+
     }
 
     public static GlobalPreferences getInstance(){
@@ -24,6 +40,16 @@ public class GlobalPreferences {
             mInstance = new GlobalPreferences();
         }
         return mInstance;
+    }
+
+    public void init(Context context){
+
+        this.context = context.getApplicationContext();
+    }
+
+    public void setOrderItemRecord(int orderId){
+
+        this.orderItemrecord.fields.iD = orderId;
     }
 
     public String getRetailerId(){
@@ -52,4 +78,43 @@ public class GlobalPreferences {
     public void setRetailerId(String value){
         retailerId = value;
     }
+
+    private Orders createNewOrder(){
+
+        Orders order = new Orders();
+        OrderRecord newOrderRecord = new OrderRecord();
+        newOrderRecord.orderFields.status="UNCONFIRMED";
+        DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+        Date date = new Date();
+        newOrderRecord.orderFields.date = date.toString();
+        order.orderRecords.add(newOrderRecord);
+        return order;
+
+    }
+
+    public void sendCreateOrderRequest(){
+
+        Gson gson = new Gson();
+        String retailerJson = gson.toJson(createNewOrder());
+        StringEntity createOrderEntity = new StringEntity(retailerJson, "UTF-8");
+        createOrderEntity.setContentType("application/json");
+
+        NetworkManager.post(context, ORDERS_URL_BASE, createOrderEntity, new NetworkManager.NetworkInterface() {
+            @Override
+            public void onResponse(ApiResponseWrapper baseResponse) {
+
+                if (baseResponse != null && baseResponse.isSuccess()) {
+                    Gson gson = new Gson();
+                    OrderRecord ordersNew = gson.fromJson(baseResponse.getJsonObjectResponse().toString(), OrderRecord.class);
+                    setOrderId(ordersNew.id);
+                    //set new order ID for order items
+                    setOrderItemRecord(ordersNew.orderFields.iD);
+
+
+                }
+            }
+        });
+
+
 }
+    }
