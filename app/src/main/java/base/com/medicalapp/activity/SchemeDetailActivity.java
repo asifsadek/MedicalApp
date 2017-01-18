cb641c329d53a46d97c34be6f8af2119e8bd62e4
vn90094 <ramachandran.ravisankar@heb.com>
2017-01-18 10:47:22 -0600

    Add Scheme method

--- app/src/main/java/base/com/medicalapp/activity/SchemeDetailActivity.java
+++ app/src/main/java/base/com/medicalapp/activity/SchemeDetailActivity.java
@@ -2,6 +2,7 @@ package base.com.medicalapp.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 import com.google.gson.Gson;
@@ -12,7 +13,7 @@ import base.com.medicalapp.manager.NetworkManager;
 import base.com.medicalapp.model.SchemeFields;
 import base.com.medicalapp.model.SchemeRecord;
 
-public class SchemeDetailActivity extends AppCompatActivity {
+public class SchemeDetailActivity extends AppCompatActivity  implements View.OnClickListener{
 
     private static final String Schemes_URL_BASE
             = "Schemes/";
@@ -68,6 +69,7 @@ public class SchemeDetailActivity extends AppCompatActivity {
         schemeExpiryDateTextView.setText(dateToDisplay(schemeFields.expiryDate));
         schemeTypeTextView.setText(schemeFields.type);
         schemeDescriptionTextview.setText(schemeFields.description);
+        setOnclickListener();
 
     }
 
@@ -75,6 +77,30 @@ public class SchemeDetailActivity extends AppCompatActivity {
 
         String date = dateFromJSON.substring(8,10)+"/"+dateFromJSON.substring(5,7)+"/"+dateFromJSON.substring(0,4);
         return date;
+    }
+
+    private void setOnclickListener() {
+
+        findViewById(R.id.buttonAddScheme).setOnClickListener(this);
+
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+
+            case R.id.buttonAddCart:
+                addSchemeToCart();
+                break;
+
+        }
+    }
+
+    public void addSchemeToCart(){
+
+        //
+
     }
 
 }
